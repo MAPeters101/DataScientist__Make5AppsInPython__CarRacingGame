e6571527784406c3d8d72cf6838c9e181d3fb8f8
Mark Peters <mapeters101@earthlink.net>
2025-01-30 17:22:35 -0500

    lesson018 Load images, game icon and more game variables.

--- racing_car_main.py
+++ racing_car_main.py
@@ -20,8 +20,20 @@ Display = pygame.display.set_mode((width,height)) # Display surface object
 pygame.display.set_caption("The Car Racing")
 clock = pygame.time.Clock()
 
+CarImg = pygame.image.load("images/car.png")
+RoadImg1 = pygame.image.load("images/road1.png")
+TreeImg1 = pygame.image.load("images/longtree1.png")
+TreeImg2 = pygame.image.load("images/longtree2.png")
+Bugatti = pygame.image.load("images/bugatti.png")
+GameIcon = pygame.image.load("images/gameicon.png")
+pygame.display.set_icon(GameIcon)
 
+life = 2
+Previous_Score = DodgeCars(Display)
+Previous_Score.Previous_Score()
 
+EndGame = False
+GamePaused = False
 
 
 
